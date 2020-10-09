6ee22e3ddbf95f760b0d5ccf8639be0de986c63c
Agasiants <Andrei.Agasiants@unilever.com>
2020-10-09 16:52:10 +0300

    1.0.1

--- LAUNCH_sap-anaplan.bat
+++
@@ -1,3 +0,0 @@
-"C:\Users\andrei.agasiants\AppData\Local\Programs\Python\Python38\python.exe" "C:\Users\andrei.agasiants\OneDrive - Unilever\Desktop\Scripts\sap-anaplan-file-creator\script.py"
-timiout 3 >nul
-exit
